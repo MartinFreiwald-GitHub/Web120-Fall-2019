87eb1fd7209046da3b253f7271a3c9124bf24b23
MartinFreiwald-GitHub <MartinFreiwald@yahoo.com>
2019-10-30 20:13:01 -0700

    Portal page is my index,html & table.php
    Changed contact link identification names and added table.php file too

---
+++ table.php
@@ -0,0 +1,57 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Basic HTML Table</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>name</th>
+    <th>descriptions</th> 
+    <th>main ingredients</th>
+    <th>cost</th>
+  </tr>
+  <tr>
+    <td>pizza</td>
+    <td>peperoni</td>
+    <td>cheese, peperoni's, tomatoe sause</td>
+    <td>$12</td>
+  </tr>
+  <tr>
+    <td>Rump Roast</td>
+    <td>The Rump part of the Steer</td>
+    <td>Beef</td>
+    <td>$13</td>
+  </tr>
+  <tr>
+    <td>French Style Stew</td>
+    <td>Meat and vegtable stew, cut to french portions</td>
+    <td>Beef cuts, Celery, Carrots, Onions, pepper, parsnips, green onions, pototoe, tomatoe, cabage</td>
+    <td>$14</td>
+  </tr>
+  <tr>
+    <td>Apple Pie</td>
+    <td>Canned and fresh cut apples</td>
+    <td>Apples, pectine sause</td>
+    <td>$5</td>
+  </tr>
+  <tr>
+    <td>Steamed vegtables</td>
+    <td>vegables cut to size</td>
+    <td>Brockly,Califlower,carrots,celery,peas,green beans</td>
+    <td>$6.50</td>
+  </tr>
+  <tr>
+    <td>Ice Cream</td>
+    <td>low fat & no sugar</td>
+    <td>Splenda, non-fat cream</td>
+    <td>$2</td>
+  </tr>
+</table>
+
+</body>
+</html>
