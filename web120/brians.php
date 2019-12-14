835c5865e959bf14f47a59081132f9ae50ed3979
MartinFreiwald-GitHub <MartinFreiwald@yahoo.com>
2019-12-13 17:52:10 -0800

    add from final project due date

---
+++ web120/brians.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en"><head>
+     <title>Devine Precious Stone Company</title>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <!--<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="css/brians.css">
+ </head>       
+ <body>
+       
+     <h1><a href="brians.php">Devine Precious Stone Company - Home Page</a></h1>
+    <header>
+     <nav>
+     <ul class="topnav" id="myTopnav">
+       <li><a href="brians.php"><p>Home</p></a></li>
+       <li><a href="gems.php"><p>Gems</p></a></li>
+       <li><a href="diamonds.php"><p>Diamonds</p></a></li>
+       <li><a href="precious_stones.php"><p>Precious Stones</p></a></li>
+       <li><a href="fp/brians.php"><p>Final Project</p></a></li>
+     </ul> 
+    </nav>
+    <hr>
+  </header>
+   <!-- START WRAPPER -->
+ 
+   <main class="wrapper">
+
+  <!-- START LEFT COL -->
+  <section>
+    <a target="_blank" href="images/img_blackdiamond.jpg"> <!-- The Attribute for the thumbnail to be inside body -->
+    <img src="images/img_blackdiamond.jpg" alt="img_blackdiamond"></a>
+    <a target="_blank" href="images/img_ocean_blue_zircon_jem.jpg"> <!-- The Attribute for the thumbnail to be inside body -->
+    <img src="images/img_ocean_blue_zircon_jem.jpg" alt="img_ocean_blue_zircon_jem"></a>
+    <a target="_blank" href="images/img_ruby_bright.jpg"> <!-- The Attribute for the thumbnail to be inside body -->
+    <img src="images/img_ruby_bright.jpg" alt="img_ruby_bright"></a>
+    <a target="_blank" href="images/img_divine_stone_alone.jpg"> <!-- The Attribute for the thumbnail to be inside body -->
+    <img src="images/img_divine_stone_alone.jpg" alt="img_divine_stone_alone"></a>   
+               
+    <h2 class="pageID">Welcome</h2>
+<!--
+     <img class="desktop" src="images/desktop.jpg" alt="Photo of martin">
+       
+     <img class="tablet" src="images/tablet.jpg" alt="Photo of martin">
+       
+     <img class="phone" src="images/phone.jpg" alt="Photo of martin">
+  -->
+       
+       <h3 class="subheader">Millions of years to create.  Just for your eyes.</h3>
+        <p><b>Quality of stones is our main interest.  I work hard to bring out the best in stones here.</b></p>
+        <p><b>Just look at these beauties!</b></p>
+          
+              <!--NON-BREAKING-SPACE = &nbsp; do not use this-->
+  </section>
+     <!-- END OF LEFT  -->
+     <!-- START RIGHT COL -->
+  <aside>
+    <h4>Need a stone cut or mounted?</h4>
+       
+      <p>Services such as setting stones and cutting services are arranged for your convenience.</p> 
+
+      <a target="_blank" href="images/img_types_diamonds.jpg"> <!-- The Attribute for the thumbnail to be inside body -->
+      <img src="images/img_types_diamonds.jpg" alt="img_types_diamonds"></a>
+  </aside>
+
+     <!-- END RIGHT COL -->
+
+    <!-- START Footer -->
+
+  <footer>
+    <p><small>&copy; 2019 - by <a href="contact.php" target="_blank">Contact US </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+  </footer>
+  </main>
+
+  <?php
+       /*  this is my php tag area to create this file extension for all of this site */          
+  ?>
+
+  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->   
+  <script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+  </script> 
+</body>
+</html>
--- web120/contact.php
+++ web120/contact.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en"><head>
-     <title>(Martin Freiwald): WEB120 Portal Website</title>
+     <title>Devine Precious Stone Company</title>
   <meta charset="utf-8">
   <meta name="robots" content="noindex,nofollow">
   <meta name="viewport" content="width=device-width">
@@ -9,18 +9,17 @@
  </head>
  <body>
      <header>
-      <h1>Martin Freiwald: WEB120 Portal</h1>
-          <nav> 
-              <ul class="topnav" id="myTopnav">
-                <li><a href="index.php">Welcome</a></li>
-                <li><a href="big/index.php">Big</a></li>
-                <li><a href="aia.php">AIA</a></li>
-                <li><a href="flowchart.php">Flowchart/Layout</a></li>
-                <li><a href="fp/index.php">Final Project</a></li>
-                <li><a href="contact.php">Contact - Martin Freiwald</a></li>
-                <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-              </ul>
-          </nav>
+     <h1><a href="brians.php">Devine Precious Stone Company Contact</a></h1>
+    <header>
+     <nav>
+     <ul class="topnav" id="myTopnav">
+       <li><a href="brians.php"><p>Home</p></a></li>
+       <li><a href="gems.php"><p>Gems</p></a></li>
+       <li><a href="diamonds.php"><p>Diamonds</p></a></li>
+       <li><a href="precious_stones.php"><p>Precious Stones</p></a></li>
+       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+     </ul>
+     </nav>
      </header>
     
 <main class="wrapper">
@@ -38,9 +37,9 @@
 
     include 'includes/contact_include.php'; #site keys & code here
 
-    $toAddress = "MartinFreiwald708@gmail.com";  //place your/your client's email address here
-    $toName = "Martin"; //place your client's name here
-    $website = "www.mileagegrinder.com/web120";  //place NAME of your client's website
+    $toAddress = "helmvrechtbrian7@gmail.com";  //place your/your client's email address here
+    $toName = "Brian"; //place your client's name here
+    $website = "www.mileagegrinder.com/web120/brians.html";  //place NAME of your client's website
 
     echo loadContact('simple.php');#demonstrates a simple contact form
     //echo loadContact('multiple.php');#demonstrates multiple form elements
@@ -48,7 +47,7 @@
 ?>
 
      <footer>
-      <p><small>© 2019 by <a href="contact.php">Contact (Martin Freiwald) </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+      <p><small>© 2019 by <a href="contact.php">Contact - US </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
     </footer>
   </main>
   <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->   
